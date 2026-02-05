a73cddebcaa8e039515cd6c93aa5be6cc769c7c8
Ashwini <bhawar.ashwini2892@gmail.com>
2026-02-05 10:45:39 -0500

    Updated runner

--- src/test/java/runner/TestRunnerChrome.java
+++ src/test/java/runner/TestRunnerChrome.java
@@ -8,7 +8,7 @@ import context.BrowserContext;
 @CucumberOptions (
     features = "src/test/resources/features",
     glue = "steps",
-    tags = "@functional",
+    tags = "@login",
     plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber-report-chrome.html",
